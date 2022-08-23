250047fb8414287fde148c7894d8f61564105644
lvyihu <1720427782@qq.com>
2022-08-23 16:03:25 +0800

    添加一些配置

--- vue2-admin/vue.config.js
+++ vue2-admin/vue.config.js
@@ -13,4 +13,21 @@ module.exports = {
       .set("components", resolve("src/components"))
       .set("views", resolve("src/views"));
   },
+  devServer: {
+    open: true,
+    host: '127.0.0.1',
+    port: 8080,
+    https: false,
+    //以上的ip和端口是我们本机的;下面为需要跨域的
+    proxy: { //配置跨域
+      '/icvs2': {
+        target: 'http://172.22.90.1:9911/icvs2', //这里后台的地址模拟的;应该填写你们真实的后台接口
+        ws: true,
+        changOrigin: true, //允许跨域
+        pathRewrite: {
+          '^/icvs2': '' //请求的时候使用这个api就可以
+        }
+      },
+    }
+  }
 }
