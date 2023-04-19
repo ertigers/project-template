090120b29649db8d2c5c55a935f7192305785ea1
lvyihu <1720427782@qq.com>
2023-04-19 15:35:50 +0800

    添加demo

--- webpack-demo/src/function/index.js
+++
@@ -1,7 +0,0 @@
-const test = ()=> {
-  console.log('这里是utils/index文件');
-}
-
-const name = '吕宜虎'
-
-export { test, name }
--- webpack-demo/src/index.js
+++
@@ -1,7 +0,0 @@
-import { test, name } from './function/index.js'
-
-const webcu2plugin = {
-  test, name
-}
-
-export default webcu2plugin
